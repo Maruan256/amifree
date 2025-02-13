82ef3fb74dd895d9989c1d2c4fa61cbf84657e7a
Maruan256 <maruanpaschen@gmail.com>
2025-02-13 19:58:17 +0100

    try the script in head directly

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Head from 'next/head';
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Is this east Berlin or west Berlin?',
@@ -19,11 +20,16 @@ export default function RootLayout({
     <html lang="en">
       <Head>
       <link rel="icon" href="/favicon.ico" />
+      <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7556699808711569`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
       </Head>
       <body>
         {children}
         <Analytics />
-      <GoogleAdsense pId="7556699808711569"></GoogleAdsense>
       </body>
     </html>
   );
